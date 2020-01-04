44c50475f11c80566a85498f953beed63b712843
Othmane SENOUCI <o.senouci@socialouts.com>
2020-01-04 20:02:07 +0100

    Added a basic class with a main entry point

--- basics_java/src/Main.java
+++ basics_java/src/Main.java
@@ -1,2 +1,17 @@
+/**
+ * public means that the class can be accessible form the outside.
+ */
 public class Main {
+
+
+    /**
+     * public: Access control keyword [public, protected, private]
+     * Static: Static vs. regular method and variables
+     * void: Data type. Means nothing. Here the return type is nothing since the method does not return anything
+     * main: is the name of the function/method. Here main access point is a static function called main()
+     * String[] args -> Is the list of argument we pass to the function using the command line.
+     */
+    public static void main(String[] args) {
+        System.out.print("Helloooooo");
+    }
 }
